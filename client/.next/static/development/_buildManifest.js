bca2407d59f8ef7d52074d97d7a72607e378d602
Azlina Yeo <azlinayeo@Azlinas-MBP.lan>
2020-08-03 12:08:38 +0100

    gitignore

--- client/.next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/register":["static\u002Fcss\u002Fstyles.chunk.css","static\u002Fchunks\u002Fstyles.js","static\u002Fchunks\u002Fpages\u002Fregister.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
