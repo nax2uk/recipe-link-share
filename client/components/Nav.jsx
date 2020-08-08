e016561d55382868ddbf3a05a7172b9876f2dee1
Azlina Yeo <azlinayeo@Azlinas-MBP.lan>
2020-08-08 12:15:41 +0100

    refactored code out of Layout

---
+++ client/components/Nav.jsx
@@ -0,0 +1,56 @@
+import { isAuthenticated, logout } from '../utils/auth';
+import Head from 'next/head';
+import Link from 'next/link';
+const Nav = () => {
+    const user = isAuthenticated();
+    return (
+        <ul className="nav nav-tabs bg-warning">
+            <li className="nav-item">
+                <Link href="/">
+                    <a className="nav-link text-dark">Home</a>
+                </Link>
+            </li>
+            {!user &&
+                <>
+                    <li className="nav-item">
+                        <Link href="/login">
+                            <a className="nav-link text-dark">Login</a>
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link href="/register">
+                            <a className="nav-link text-dark">Register</a>
+                        </Link>
+                    </li>
+                </>
+            }
+            {user &&
+                <li className="nav-item">
+                    <a
+                        className="nav-link text-dark"
+                        onClick={logout}
+                    >Logout</a>
+                </li>
+            }
+            {user && user.role === 'admin'
+                &&
+                <li className="nav-item" ml-auto>
+                    <Link href="/admin">
+                        <a className="nav-link text-dark">Admin</a>
+                    </Link>
+                </li>
+            }
+
+            {user && user.role === 'subscriber'
+                &&
+                <li className="nav-item" ml-auto>
+                    <Link href="/user">
+                        <a className="nav-link text-dark">User</a>
+                    </Link>
+                </li>
+            }
+
+        </ul>
+    );
+};
+export default Nav;
