71d1e74794064a4645bf6168475913a678e37bda
Azlina Yeo <azlinayeo@Azlinas-MBP.lan>
2020-08-11 17:10:31 +0100

    added getInitial Props

--- client/pages/user/index.js
+++ client/pages/user/index.js
@@ -1,11 +1,31 @@
 import Layout from '../../components/Layout';
+import { API } from '../../config';
+import { getCookie } from '../../utils/auth';
+import axios from 'axios';
 
-const User = () => {
+const User = ({ user }) => {
     return (
         <Layout>
-            <h1>User</h1>
+            <h1>{JSON.stringify(user)}</h1>
         </Layout>
 
     );
+}
+
+User.getInitialProps = async () => {
+    const token = getCookie('token');
+    try {
+        const response = await axios.get(`${API}/user`, {
+            headers: {
+                authorization: `Bearer ${token}`,
+                contentType: 'application/json'
+            }
+        })
+        return { user: response.data }
+    } catch (error) {
+        if (error.response.status === 401) {
+            return { user: 'no user' };
+        }
+    }
 }
 export default User;
