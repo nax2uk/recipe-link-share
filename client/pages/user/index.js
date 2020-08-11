8e6053c1d565b2077859c5ba944b36be0d06dda2
Azlina Yeo <azlinayeo@gmail.com>
2020-08-11 20:54:04 +0100

    refactored getInitial props to HOC withUser

--- client/pages/user/index.js
+++ client/pages/user/index.js
@@ -1,31 +1,29 @@
 import Layout from '../../components/Layout';
-import { API } from '../../config';
-import { getCookie } from '../../utils/auth';
-import axios from 'axios';
+import withUser from '../withUser';
 
-const User = ({ user }) => {
+const User = ({ user, token }) => {
     return (
         <Layout>
-            <h1>{JSON.stringify(user)}</h1>
+            <h1>{JSON.stringify(token)}</h1>
         </Layout>
 
     );
 }
 
-User.getInitialProps = async (context) => {
-    const token = getCookie('token', context.req);
-    try {
-        const response = await axios.get(`${API}/user`, {
-            headers: {
-                authorization: `Bearer ${token}`,
-                contentType: 'application/json'
-            }
-        })
-        return { user: response.data }
-    } catch (error) {
-        if (error.response.status === 401) {
-            return { user: 'no user' };
-        }
-    }
-}
-export default User;
+// User.getInitialProps = async (context) => {
+//     const token = getCookie('token', context.req);
+//     try {
+//         const response = await axios.get(`${API}/user`, {
+//             headers: {
+//                 authorization: `Bearer ${token}`,
+//                 contentType: 'application/json'
+//             }
+//         })
+//         return { user: response.data }
+//     } catch (error) {
+//         if (error.response.status === 401) {
+//             return { user: 'no user' };
+//         }
+//     }
+// }
+export default withUser(User);
