ced95ecb512ca7dd2f8a8b32d9e89ca5df91f597
Azlina Yeo <azlinayeo@gmail.com>
2020-08-12 11:32:43 +0100

    added userForgotPassword to controller methods

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,6 @@
 const { errInvalidPaths, errStatus405 } = require("./error");
 const { postLogin } = require("./login");
-const { userRegister, userActivate, userProfile } = require("./user");
+const { userRegister, userActivate, userProfile, userForgotPassword } = require("./user");
 
 module.exports = {
     errStatus405,
@@ -8,6 +8,7 @@ module.exports = {
     postLogin,
     userRegister,
     userActivate,
-    userProfile
+    userProfile,
+    userForgotPassword
 };
 
