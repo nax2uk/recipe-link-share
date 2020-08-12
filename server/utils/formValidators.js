ab2220c31663467272d470ff9a021ea58b20152e
Azlina Yeo <azlinayeo@gmail.com>
2020-08-12 11:31:46 +0100

    added form validators userForgotPassword and userResetPassword

--- server/utils/formValidators.js
+++ server/utils/formValidators.js
@@ -22,4 +22,21 @@ exports.validateLogin = [
         .withMessage('Password must be at least 6 characters long')
 ];
 
+exports.validateForgotPassword = [
+    check('email')
+        .isEmail()
+        .withMessage('Must be a valid email address'),
+
+];
+
+exports.validateResetPassword = [
+    check('newPassword')
+        .isLength({ min: 6 })
+        .withMessage('Password must be at least 6 characters long'),
+    check('resetPasswordLink')
+        .not()
+        .isEmpty()
+        .withMessage('Token is required'),
+];
+
 
