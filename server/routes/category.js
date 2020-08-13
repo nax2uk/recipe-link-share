10abbf3fa19dbec8da4b5b4a2722f0b4b5e7f38f
Azlina Yeo <azlinayeo@gmail.com>
2020-08-14 00:04:25 +0100

    added more category routes

--- server/routes/category.js
+++ server/routes/category.js
@@ -1,23 +1,27 @@
 const routerCategory = require('express').Router();
 
 // formValidators
-const { categoryCreateValidator, categoryUpdateValidator } = require('../utils/formValidators');
+const { validateAddCategory, validateUpdateCategory } = require('../utils/formValidators');
 
 // middleware
 const { runValidation } = require('../middleware/validators');
 const { requireLogin, adminMiddleware } = require('../middleware/auth');
 
 //controller
-const { postCategory, getCategory, errStatus405 } = require('../controllers');
+const { postCategory, getAllCategories, getCategoryById, removeCategoryById errStatus405 } = require('../controllers');
 
 routerCategory
     .route('/')
-    .post(categoryCreateValidator, runValidation, requireLogin, adminMiddleware, postCategory)
-    .get(getCategory)
+    .post(validateAddCategory, runValidation, requireLogin, adminMiddleware, postCategory)
+    .get(getAllCategories)
     .all(errStatus405);
 
 routerCategory
     .route('/:slug')
+    .get(getCategoryById)
+    .put(validateUpdateCategory, runValidation, requireLogin, adminMiddleware, putCategoryById)
+    .delete(requireLogin, adminMiddleware, removeCategoryById)
+    .all(errStatus405);
 
 
 
