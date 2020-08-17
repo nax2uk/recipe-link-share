84a190507d5ddc858854f7343bf3c83a801ed713
Azlina Yeo <azlinayeo@gmail.com>
2020-08-17 13:29:49 +0100

    added postCategory

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -1,7 +1,34 @@
+const Category = require('../models/category');
+const slugify = require('slugify');
+
 exports.getAllCategories = (req, res) => { }
 //postCategory, getAllCategories, getCategoryById, removeCategoryById, putCategoryById
 exports.getCategoryById = (req, res) => { }
-exports.postCategory = (req, res) => { }
+exports.postCategory = (req, res) => {
+
+    const { name, content } = req.body;
+    const slug = slugify(name);
+    const image = {
+        url: `https://via.placeholder.com/200x150.png?text=${process.env.CLIENT_URL}`,
+        key: '123'
+    };
+    console.log(req.user._id);
+    const category = new Category({ name, slug, image, content });
+    category.postedBy = req.user._id;
+
+
+    category.save((err, data) => {
+        if (err) {
+            console.log('CATEGORY CREATE ERR', err)
+            return res.status(400).json({
+                error: 'Category create failed',
+            })
+        }
+        return res.status(201).json(data);
+    })
+
+
+}
 exports.putCategoryById = (req, res) => { }
 exports.removeCategoryById = (req, res) => { }
 
