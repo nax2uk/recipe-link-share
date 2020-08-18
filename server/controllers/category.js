b92c1ff115b6da9e235e1cf4da9794f96e219ca5
Azlina Yeo <azlinayeo@gmail.com>
2020-08-18 18:22:33 +0100

    changed image to base64 in postCategory

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -23,56 +23,91 @@ exports.getAllCategories = (req, res) => {
 }
 
 exports.getCategoryById = (req, res) => { }
-
 exports.postCategory = (req, res) => {
-    let form = new formidable.IncomingForm();
-    form.parse(req, (err, fields, files) => {
+    const { name, image, content } = req.body;
+    const base64Data = new Buffer.from(image.replace(/^data:image\/\w+;base64,/, ''), 'base64');
+
+    const type = image.split(';')[0].split('/')[1];
+    const slug = slugify(name);
+    let category = new Category({ name, content, slug });
+
+    //upload image to S3
+    const params = {
+        Bucket: 'recipes-nax2uk',
+        Key: `category/${uuidv4()}.${type}`,
+        Body: base64Data,
+        ACL: 'public-read',
+        ContentEncoding: 'base64',
+        ContentType: `image/${type}`
+    }
+    s3.upload(params, (err, data) => {
         if (err) {
-            return res.status(400).json({
-                error: "Image could not upload"
+            res.status(400).json({
+                error: "Upload to S3 failed"
             })
         }
-        //console.table({err, fields, files})
-        const { name, content } = fields;
-        const { image } = files;
-        // restrict image to 2MB
-        if (image.size > 2000000) {
-            return res.status(400).json({
-                error: "Image should be less than 2MB"
-            })
-        }
-        const slug = slugify(name);
-        let category = new Category({ name, content, slug });
+        console.log('AWS UPLOAD RES DATA', data);
+        category.image.url = data.Location;
+        category.image.key = data.Key;
 
-        //upload image to S3
-        const params = {
-            Bucket: 'recipes-nax2uk',
-            Key: `category/${uuidv4()}`,
-            Body: fs.readFileSync(image.path),
-            ACL: 'public-read',
-            ContentType: 'image/jpg'
-        }
-        s3.upload(params, (err, data) => {
-            if (err) {
-                res.status(400).json({
-                    error: "Upload to S3 failed"
-                })
-            }
-            console.log('AWS UPLOAD RES DATA', data);
-            category.image.url = data.Location;
-            category.image.key = data.Key;
-
-            category.save((err, data) => {
-                if (err) return res.status(400).json({
-                    error: "Duplicate category"
-                })
-                return res.status(201).json(data);
+        category.save((err, data) => {
+            if (err) return res.status(400).json({
+                error: "Duplicate category"
             })
+            return res.status(201).json(data);
         })
-
-
     })
 }
+
+// exports.postCategory = (req, res) => {
+//     let form = new formidable.IncomingForm();
+//     form.parse(req, (err, fields, files) => {
+//         if (err) {
+//             return res.status(400).json({
+//                 error: "Image could not upload"
+//             })
+//         }
+//         //console.table({err, fields, files})
+//         const { name, content } = fields;
+//         const { image } = files;
+//         // restrict image to 2MB
+//         if (image.size > 2000000) {
+//             return res.status(400).json({
+//                 error: "Image should be less than 2MB"
+//             })
+//         }
+//         const slug = slugify(name);
+//         let category = new Category({ name, content, slug });
+
+//         //upload image to S3
+//         const params = {
+//             Bucket: 'recipes-nax2uk',
+//             Key: `category/${uuidv4()}`,
+//             Body: fs.readFileSync(image.path),
+//             ACL: 'public-read',
+//             ContentType: 'image/jpg'
+//         }
+//         s3.upload(params, (err, data) => {
+//             if (err) {
+//                 res.status(400).json({
+//                     error: "Upload to S3 failed"
+//                 })
+//             }
+//             console.log('AWS UPLOAD RES DATA', data);
+//             category.image.url = data.Location;
+//             category.image.key = data.Key;
+
+//             category.save((err, data) => {
+//                 if (err) return res.status(400).json({
+//                     error: "Duplicate category"
+//                 })
+//                 return res.status(201).json(data);
+//             })
+//         })
+
+
+//     })
+// }
 exports.putCategoryById = (req, res) => { }
 exports.removeCategoryById = (req, res) => { }
 
