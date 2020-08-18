dcb828219309be22c86af50b07f62d3cd0fc85e9
Azlina Yeo <azlinayeo@gmail.com>
2020-08-18 16:40:12 +0100

    added form and added category and images and posted to api

--- client/pages/admin/index.js
+++ client/pages/admin/index.js
@@ -1,10 +1,24 @@
 import Layout from '../../components/Layout';
 import withAdmin from '../withAdmin';
+import Link from 'next/link';
 
 const Admin = ({ user }) => {
     return (
         <Layout>
-            <h1>{JSON.stringify(user)}</h1>
+            <h1>Admin Dashboard</h1>
+            <br />
+            <div className="row">
+                <div className="col-md-4">
+                    <ul className="nav flex-column">
+                        <li className="nav-item">
+                            <Link href="/admin/category/add">
+                                <a className="nav-link">Add category</a>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-md-8"></div>
+            </div>
         </Layout>
     );
 }
