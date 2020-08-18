bf2da18eb325e199a7a9aa529c7e39ca95b6aacb
Azlina Yeo <azlinayeo@gmail.com>
2020-08-18 17:18:13 +0100

    fetch categories

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,16 @@
 import Layout from '../components/Layout';
+import axios from 'axios';
+import { API } from '../config';
 
-const Home = () => {
-    return <Layout>Hello next</Layout>
+const Home = ({ categories }) => {
+    return <Layout>{JSON.stringify(categories)}</Layout>
+}
+
+Home.getInitialProps = async () => {
+    const response = await axios.get(`${API}/category`);
+    return {
+        categories: response.data
+    }
 }
 
 export default Home;
