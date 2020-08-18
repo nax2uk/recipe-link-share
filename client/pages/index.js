ffedeed4e918f6b2cf12a525beec29de41c1fc15
Azlina Yeo <azlinayeo@gmail.com>
2020-08-18 17:52:47 +0100

    formatted with HTML

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,43 @@
 import Layout from '../components/Layout';
 import axios from 'axios';
 import { API } from '../config';
+import Link from 'next/link';
 
 const Home = ({ categories }) => {
-    return <Layout>{JSON.stringify(categories)}</Layout>
+
+    const listCategories = () => categories.map((category, idx) => (
+        <Link href="/" key={idx}>
+            <a style={{ border: '1px solid black' }} className="bg-light p-3 col-md-4">
+                <div>
+                    <div className="row">
+                        <div className="col-md-4">
+                            <img
+                                src={category.image && category.image.url}
+                                alt={category.name}
+                                style={{ width: '100px', height: 'auto' }}
+                                className="pr-3"
+                            />
+                        </div>
+                        <div className="col-md-8">
+                            <h3>{category.name}</h3>
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </Link>
+    ))
+
+    return (
+        <Layout>
+            <div className="row">
+                <div className="col-md-12">
+                    <h1 className="font-weight-bold">Browse Recipes By Category</h1>
+                    <br />
+                </div>
+            </div>
+
+            <div className="row">{listCategories()}</div>
+        </Layout>);
 }
 
 Home.getInitialProps = async () => {
