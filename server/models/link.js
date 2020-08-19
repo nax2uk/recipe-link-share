e8311316d6d916e5ed3fa807349d72c6b66a6657
Azlina Yeo <azlinayeo@gmail.com>
2020-08-19 14:39:35 +0100

    added Link Schema

---
+++ server/models/link.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+const linkSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            trim: true,
+            required: true,
+            max: 256
+        },
+        url: {
+            type: String,
+            trim: true,
+            required: true,
+            max: 256
+        },
+        slug: {
+            type: String,
+            lowercase: true,
+            required: true,
+            index: true
+        },
+        postedBy: {
+            type: ObjectId,
+            ref: 'User'
+        },
+        categories: [
+            {
+                type: ObjectId,
+                ref: 'Category',
+                required: true
+            }
+        ],
+        type: {
+            type: String,
+            default: 'Web Page'
+        },
+        clicks: { type: Number, default: 0 }
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('Link', linkSchema);
