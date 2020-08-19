765dea6f5810ee6ce55b0a245ca221ae7446523e
Azlina Yeo <azlinayeo@gmail.com>
2020-08-19 18:33:17 +0100

    edited getAllLinks code typp Link.find

--- server/controllers/link.js
+++ server/controllers/link.js
@@ -2,12 +2,31 @@ const Link = require('../models/link');
 const slugify = require('slugify');
 
 exports.addLink = (req, res) => {
-    const { title, url, categories } = req.body;
-    console.log({ title, url, categories })
+    const { title, url, categories, type } = req.body;
+    //console.table({ title, url, categories });
+
+    let link = new Link({ title, url, type });
+    link.slug = url;
+    link.postedBy = req.user._id;
+    //let arrCategories = categories && categories.split(',');
+    link.categories = categories && categories.split(',');
+
+    link.save((err, data) => {
+
+        if (err) return res.status(400).json({
+            error: "Duplicate Link",
+        })
+        res.status(201).json(data)
+    });
 }
 
 exports.getAllLinks = (req, res) => {
-
+    Link.find({}).exec((err, data) => {
+        if (err) return res.status(400).json({
+            error: 'Error getting links.'
+        })
+        res.status(200).json(data)
+    })
 }
 
 exports.getLinkBySlug = (req, res) => {
