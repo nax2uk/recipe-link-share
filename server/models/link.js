84806182211c6e34d96be6fdbd6f1f97c2d0358c
Azlina Yeo <azlinayeo@gmail.com>
2020-08-19 18:20:54 +0100

    made slug unique

--- server/models/link.js
+++ server/models/link.js
@@ -17,8 +17,8 @@ const linkSchema = new mongoose.Schema(
         },
         slug: {
             type: String,
+            unique: true,
             lowercase: true,
-            required: true,
             index: true
         },
         postedBy: {
