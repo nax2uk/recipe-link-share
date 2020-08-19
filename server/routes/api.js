921e471f33665b715a799f6cbc2bdfcb99df8efc
Azlina Yeo <azlinayeo@gmail.com>
2020-08-19 15:05:39 +0100

    added link router

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,12 +3,14 @@ const {
     routerUser,
     routerLogin,
     routerAdmin,
-    routerCategory
+    routerCategory,
+    routerLink
 } = require("../routes");
 
 routerApi.use("/user", routerUser);
 routerApi.use("/login", routerLogin);
 routerApi.use("/admin", routerAdmin);
 routerApi.use("/category", routerCategory);
+routerApi.use("/link", routerLink);
 
 module.exports = routerApi;
