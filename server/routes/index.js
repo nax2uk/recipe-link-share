31ef17e310084ec93a08a9ae4db7fab5ff60ca2f
Azlina Yeo <azlinayeo@gmail.com>
2020-08-19 14:45:27 +0100

    added routerLink

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,3 +2,4 @@ exports.routerUser = require("./user");
 exports.routerLogin = require("./login");
 exports.routerAdmin = require("./admin");
 exports.routerCategory = require("./category");
+exports.routerLink = require("./link");
