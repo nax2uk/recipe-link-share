ffe0dd9ab9ef65673bfb1a3453fc5a121a3a899c
Azlina Yeo <azlinayeo@gmail.com>
2020-08-19 15:09:40 +0100

    renamed middleware folder to middlewares

--- server/middleware/index.js
+++
@@ -1,4 +0,0 @@
-const { runValidation } = require('./validators');
-const { requireLogin, userMiddleware, adminMiddleware } = require('./auth');
-
-module.exports = { runValidation, requireLogin, userMiddleware, adminMiddleware };
