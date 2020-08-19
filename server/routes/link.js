1d99662f2d18d613a9ec777c482976df547a610d
Azlina Yeo <azlinayeo@gmail.com>
2020-08-19 15:09:17 +0100

    added routes

---
+++ server/routes/link.js
@@ -0,0 +1,25 @@
+const routerLink = require('express').Router();
+
+// validators
+const { validateAddLink, validateUpdateLink } = require('../utils/formValidators/linkValidators');
+
+//middlewares
+const { runValidation, requireLogin, userMiddleware } = require('../middleware');
+
+// controllers
+const { addLink, getAllLinks, getLinkBySlug, updateLinkBySlug, deleteLinkBySlug, errStatus405 } = require('../controllers');
+
+// routes
+routerLink
+    .route('/')
+    .post(validateAddLink, runValidation, requireLogin, userMiddleware, addLink)
+    .get(getAllLinks)
+    .all(errStatus405);
+
+routerLink.route('/:slug')
+    .get(getLinkBySlug)
+    .put(validateUpdateLink, runValidation, requireLogin, userMiddleware, updateLinkBySlug)
+    .delete(requireLogin, userMiddleware, deleteLinkBySlug)
+    .all(errStatus405);
+
+module.exports = routerLink;
