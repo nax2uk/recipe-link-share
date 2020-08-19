12e50661095fb93e481d358603670fa5f77a4270
Azlina Yeo <azlinayeo@gmail.com>
2020-08-19 15:23:09 +0100

    added link controllers

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,20 +2,13 @@ const { errInvalidPaths, errStatus405 } = require("./error");
 const { postLogin } = require("./login");
 const { userRegister, userActivate, userProfile, userForgotPassword, userResetPassword } = require("./user");
 const { postCategory, getAllCategories, getCategoryById, removeCategoryById, putCategoryById } = require('./category');
+const { addLink, getAllLinks, getLinkBySlug, updateLinkBySlug, deleteLinkBySlug } = require('./link');
 
 module.exports = {
-    errStatus405,
-    errInvalidPaths,
+    errStatus405, errInvalidPaths,
     postLogin,
-    userRegister,
-    userActivate,
-    userProfile,
-    userForgotPassword,
-    userResetPassword,
-    postCategory,
-    getAllCategories,
-    getCategoryById,
-    removeCategoryById,
-    putCategoryById
+    userRegister, userActivate, userProfile, userForgotPassword, userResetPassword,
+    postCategory, getAllCategories, getCategoryById, removeCategoryById, putCategoryById,
+    addLink, getAllLinks, getLinkBySlug, updateLinkBySlug, deleteLinkBySlug
 };
 
