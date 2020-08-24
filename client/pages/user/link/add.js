c6ee8884a62e7cf0f175d023fb39b8d40059585f
Azlina Yeo <azlinayeo@gmail.com>
2020-08-24 22:53:25 +0100

    added state to add pages

--- client/pages/user/link/add.js
+++ client/pages/user/link/add.js
@@ -1,2 +1,38 @@
-const Add = () => <p>add link page</p>
+const Add = () => {
+    const [state, setState] = useState({
+        title: '',
+        url: '',
+        categories: [],
+        loadedCategories: [],
+        success: '',
+        error: '',
+        type: '',
+        medium: ''
+    });
+
+    const { title, url, categories, loadedCategories, success, error, type, medium } = state;
+
+    // load categories when component mounts using useEffect
+    useEffect(() => {
+        loadCategories();
+    }, [success]);
+
+    const loadCategories = async () => {
+        const response = await axios.get(`${API}/category`);
+        setState({ ...state, loadedCategories: response.data });
+    };
+
+    return (
+        <Layout>
+            <div className="row">
+                <div className="col-md-12">
+                    <h1>Submit Link/URL</h1>
+                    <br />
+                    {JSON.stringify(loadedCategories)}
+                </div>
+            </div>
+        </Layout>
+    );
+};
+
 export default Add;
